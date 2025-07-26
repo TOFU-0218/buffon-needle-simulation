8283166495adde8f92c2b6cdb0544a289296ac93
TofuMasume <tofu.masume@gmail.com>
2025-07-26 22:41:24 +0900

    add: 時間計測機能

--- naive/simulation.c
+++ naive/simulation.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<time.h>
 
 double GenerateNeedle(double interval, double length);
 double GetRandom(double max);
+
+static double diff_in_sec(const struct timespec *start,
+                          const struct timespec *end){
+        return (end->tv_sec - start->tv_sec) +
+               (end->tv_nsec - start->tv_nsec) * 1e-9;
+}
+
 
 int main(){
         int seed, loopN;
@@ -17,14 +25,23 @@ int main(){
         printf("times: ");
         scanf("%d", &loopN);
         srand(seed);
+        struct timespec t_start, t_end;
+        clock_gettime(CLOCK_MONOTONIC, &t_start);
+        int hits = 0;
         for(int i=0; i<loopN; i++){
                 double tipPosition = GenerateNeedle(interval, length);
                 int isOver = 0;
                 if(tipPosition >= interval / 2){
                         isOver = 1;
+                        hits++;
                 }
                 printf("%d : %lf\n", isOver, tipPosition);
         }
+        clock_gettime(CLOCK_MONOTONIC, &t_end);
+        double elapsed = diff_in_sec(&t_start, &t_end);
+        printf("===========\n");
+        printf("Wall time: %.6f sec\n", elapsed);
+
 }
 
 // [0, max)の一様乱数
