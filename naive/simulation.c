08b20b574cff9cf746fa3ed9eb83fa36d28ea2cb
TofuMasume <tofu.masume@gmail.com>
2025-07-27 00:24:00 +0900

    fix: いくつかコメントアウトを削除

--- naive/simulation.c
+++ naive/simulation.c
@@ -35,13 +35,9 @@ int main(){
         printf("\n----------\n");
         for(int i=0; i<loopN; i++){
                 double tipPosition = GenerateNeedle(interval, length);
-                // int isOver = 0;
                 if(tipPosition >= interval / 2){
-                        // isOver = 1;
                         hits++;
                 }
-                // printf("%d : %lf\n", isOver, tipPosition);
-                // PrintProgressBar(i, loopN);
                 if(i >= (int)progress*ten_percent){
                         printf("#");
                         progress++;
@@ -51,7 +47,6 @@ int main(){
         clock_gettime(CLOCK_MONOTONIC, &t_end);
         printf("\n");
         double elapsed = diff_in_sec(&t_start, &t_end);
-        // printf("===========\n");
         printf("Wall time: %.6f sec\n", elapsed);
         double pi_est = 2.0 * length * loopN / (interval * hits);
         printf("Estimated pi = %.6f\n", pi_est);
@@ -68,19 +63,4 @@ double GenerateNeedle(double interval, double length){
         double sin_theta = sin(GetRandom(M_PI/2.0));
         double projection = (length / 2.0) * sin_theta;
         return position + projection;
-}
-
-// 進捗バーを描画
-void PrintProgressBar(int current, int total){
-        const int bar_width = 50;
-        double ratio = (double)current / total;
-        int filled = (int)(ratio * bar_width);
-
-        // キャリッジリターンで行頭
-        printf("\r[");
-        for(int i=0; i<filled; i++) printf("=");
-        for(int i = filled; i<bar_width; i++) printf(" ");
-        printf("] %3.0f%%", ratio*100);
-
-        fflush(stdout);
 }
