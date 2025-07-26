d5a4a377ce210f0ced011d1e27351b5c9fff8e61
TofuMasume <tofu.masume@gmail.com>
2025-07-26 22:06:31 +0900

    add: 回数変更機能を追加

--- naive/simulation.c
+++ naive/simulation.c
@@ -6,7 +6,7 @@ double GenerateNeedle(double interval, double length);
 double GetRandom(double max);
 
 int main(){
-        int seed;
+        int seed, loopN;
         double length, interval;
         printf("seed:");
         scanf("%d", &seed);
@@ -14,10 +14,17 @@ int main(){
         scanf("%lf", &length);
         printf("interval:");
         scanf("%lf", &interval);
+        printf("times: ");
+        scanf("%d", &loopN);
         srand(seed);
         int i;
-        for(i=0; i<10; i++){
-                printf("%lf\n", GenerateNeedle(interval, length));
+        for(i=0; i<loopN; i++){
+                double tipPosition = GenerateNeedle(interval, length);
+                int isOver = 0;
+                if(tipPosition >= interval / 2){
+                        isOver = 1;
+                }
+                printf("%d : %lf\n", isOver, tipPosition);
         }
 }
 
@@ -29,7 +36,7 @@ double GetRandom(double max){
 // 針の先端位置を生成
 double GenerateNeedle(double interval, double length){
         double position = GetRandom(interval / 2.0);
-        double sin_theta = sin(GetRandom(M_PI));
+        double sin_theta = sin(GetRandom(M_PI/2.0));
         double projection = (length / 2.0) * sin_theta;
         return position + projection;
 }
